66f22ca863cddfffaa750adb9552af19dffcede1
dhanu98 <67949452+dhanu98@users.noreply.github.com>
2023-01-31 09:21:50 +0530

    Update settings.py

--- News Website Using Python Django/News Website Using Python/settings.py
+++ News Website Using Python Django/News Website Using Python/settings.py
@@ -11,7 +11,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'x8pjg4!@v!csuw)1d-%&z)=24^^chn@=xncs#$9!@@zw0c$)ls'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
